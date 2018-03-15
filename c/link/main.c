d492dc84fb9d13b91f688c8f41285b3e96444c66
sigma318 <liaokunnan@huawei.com>
2018-03-15 23:54:46 +0800

    Create main.c

---
+++ c/link/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+#include "dlist.h"
+
+void test_p(int **pint)
+{
+    int *pp = *pint;
+    printf("pp:%p, *pint:%p, %d\n", pp, *pint, pint);
+}
+
+int main(void)
+{
+    printf("start here\n");
+    sl_test();
+
+    dl_list_test();
+    printf("end here\n");
+
+    int *node = NULL;
+    int *tmp = node;
+    int a = 30;
+    int *ppp = 100;
+
+    printf("ppp:%p, &ppp:%d\n", ppp, &ppp);
+
+    printf("node: %p, %d, tmp:%p, %d\n", node, &node, tmp, &tmp);
+
+    test_p(&node);
+    node = &a;
+    printf("node: %p, %d, tmp:%p, %d\n", node, &node, tmp, &tmp);
+
+    printf("a addr:%d\n", &a);
+    test_p(&node);
+
+    return 0;
+}
